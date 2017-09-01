e8a69104f37cf914afedd1c2871f49227c96e284
Nedqlkov <fbinnzhivko@gmail.com>
2017-09-01 12:33:37 +0300

    old exam new solutions need for forum comment

--- 01.Short Fundamentals/44.PRACTICAL EXAM - 09.07.2017/03.00 Regexmon/Program.cs
+++ 01.Short Fundamentals/44.PRACTICAL EXAM - 09.07.2017/03.00 Regexmon/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _03._00_Regexmon
+using System.Text.RegularExpressions;
+public class Program
 {
-    class Program
+    public static void Main() // 100/100
     {
-        static void Main(string[] args)
+        string input = Console.ReadLine();
+
+        string patternDidi = @"[^A-Za-z-]+";
+        string patternBojo = @"[A-Za-z]+-[A-Za-z]+";
+
+        Regex regexDidi = new Regex(patternDidi);
+        Regex regexBojo = new Regex(patternBojo);
+
+        while (true)
         {
+            Match didiMatch = regexDidi.Match(input);
+            if (!didiMatch.Success)
+            {
+                break;
+            }
+            Console.WriteLine(didiMatch.Value);
+
+            int firstSymbolDidi = didiMatch.Index;
+            input = input.Substring(firstSymbolDidi + didiMatch.Length);
+
+            Match bojoMatch = regexBojo.Match(input);
+            if (!bojoMatch.Success)
+            {
+                break;
+            }
+            Console.WriteLine(bojoMatch.Value);
+
+            int firstSymbolBojo = bojoMatch.Index;
+            input = input.Substring(firstSymbolBojo + bojoMatch.Length);
         }
     }
 }
--- 01.Short Fundamentals/44.PRACTICAL EXAM - 09.07.2017/04.00 Pokemon Evolution/Program.cs
+++ 01.Short Fundamentals/44.PRACTICAL EXAM - 09.07.2017/04.00 Pokemon Evolution/Program.cs
@@ -1,15 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _04._00_Pokemon_Evolution
+class PokemonEvolution
 {
-    class Program
+    static void Main() //  100/100
     {
-        static void Main(string[] args)
+        Dictionary<string, List<Evolution>> data = new Dictionary<string, List<Evolution>>();
+
+        string input = Console.ReadLine();
+
+        while (input != "wubbalubbadubdub")
         {
+            string[] tokens = input.Split(new char[] { '-', '>', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string pokemonName = tokens[0];
+
+            if (tokens.Length == 1 && data.ContainsKey(pokemonName))
+            {
+                PrintPokemons(pokemonName, data);
+            }
+            else
+            {
+                try
+                {
+                    string evolutionType = tokens[1];
+                    long evolutionIndex = long.Parse(tokens[2]);
+
+                    Evolution evolution = new Evolution(evolutionType, evolutionIndex);
+
+                    if (!data.ContainsKey(pokemonName))
+                    {
+                        data.Add(pokemonName, new List<Evolution>());
+                    }
+                    data[pokemonName].Add(evolution);
+                }
+                catch
+                {
+
+                }
+
+            }
+            input = Console.ReadLine();
+        }
+
+        foreach (var pokemonData in data)
+        {
+            string pokemonName = pokemonData.Key;
+            List<Evolution> evolutions = pokemonData.Value;
+
+            Console.WriteLine($"# {pokemonName}");
+
+            foreach (Evolution evolutionData in evolutions.OrderByDescending(e => e.EvolutionIndex))
+            {
+                string evolutionType = evolutionData.EvolutionType;
+                long evolutionIndex = evolutionData.EvolutionIndex;
+
+                Console.WriteLine($"{evolutionType} <-> {evolutionIndex}");
+            }
+        }
+
+    }
+
+    private static void PrintPokemons(string pokemonName, Dictionary<string, List<Evolution>> data)
+    {
+        foreach (var item in data.Where(k => k.Key == pokemonName))
+        {
+            Console.WriteLine($"# {pokemonName}");
+
+            List<Evolution> evolutionData = item.Value;
+
+            foreach (var evolution in evolutionData)
+            {
+                string evolutionType = evolution.EvolutionType;
+                long evolutionIndex = evolution.EvolutionIndex;
+
+                Console.WriteLine($"{evolutionType} <-> {evolutionIndex}");
+            }
+        }
+    }
+    class Evolution
+    {
+        public string EvolutionType { get; set; }
+        public long EvolutionIndex { get; set; }
+
+        public Evolution(string evolutionType, long evolutionIndex)
+        {
+            this.EvolutionType = evolutionType;
+            this.EvolutionIndex = evolutionIndex;
         }
     }
 }
