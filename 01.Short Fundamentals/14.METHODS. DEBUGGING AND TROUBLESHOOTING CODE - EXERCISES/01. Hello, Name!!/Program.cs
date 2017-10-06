c2f5f4c5151e149c05fbddb3d94ce2e54f0c80d7
Tuscann <fbinnzhivko@gmail.com>
2017-10-07 00:53:55 +0300

    rename one file

---
+++ 01.Short Fundamentals/14.METHODS. DEBUGGING AND TROUBLESHOOTING CODE - EXERCISES/01. Hello, Name!!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+
+    static void printName(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+    static void Main()  // 100/100
+    {
+        string name = Console.ReadLine();
+        printName(name);
+    }
+}
